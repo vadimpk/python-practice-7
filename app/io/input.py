492ddd6021cd5fac0df511b4b173337937b3c2a1
vadimpk <vadyman.pk@gmail.com>
2024-03-18 18:11:16 +0200

    add docstring annotation

--- app/io/input.py
+++ app/io/input.py
@@ -2,13 +2,24 @@ import pandas as pd
 
 
 def read_from_console():
-    return input("Please enter your text: ")
+    """Зчитує текст, введений користувачем з консолі."""
+    return input("Будь ласка, введіть текст: ")
 
 
 def read_from_file_builtin(filepath):
+    """Зчитує текст з файлу за допомогою вбудованих можливостей Python.
+
+    Args:
+        filepath (str): Шлях до файлу для зчитування.
+    """
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
 
 
 def read_from_file_pandas(filepath):
+    """Зчитує дані з файлу у форматі CSV за допомогою бібліотеки pandas.
+
+    Args:
+        filepath (str): Шлях до файлу CSV для зчитування.
+    """
     return pd.read_csv(filepath)
--- app/io/output.py
+++ app/io/output.py
@@ -1,7 +1,18 @@
 def print_to_console(text):
+    """Виводить текст у консоль.
+
+    Args:
+        text (str): Текст для виводу.
+    """
     print(text)
 
 
 def write_to_file_builtin(filepath, text):
+    """Записує текст до файлу за допомогою вбудованих можливостей Python.
+
+    Args:
+        filepath (str): Шлях до файлу для запису.
+        text (str): Текст, який потрібно записати.
+    """
     with open(filepath, 'w', encoding='utf-8') as file:
         file.write(text)
